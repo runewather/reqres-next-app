232796d72e975c26d2cc7f0db5e8cece3e92f739
Victo Principe <victo_10_@hotmail.com>
2022-07-05 17:10:52 -0300

    Added AppBar component

---
+++ components/Navbar/AppBar.tsx
@@ -0,0 +1,11 @@
+import styles from "./AppBar.module.css";
+
+const AppBar: React.FC = () => {
+  return (
+    <header className={styles.appBar}>
+      <h1>Regres.in</h1>
+    </header>
+  )
+}
+
+export default AppBar;
---
+++ layouts/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout: React.FC = () => (
+  <></>
+)
+
+export default Layout;
